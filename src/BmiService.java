efd17dd760529c0eec46daf44085da9e56999886
Nata R <natasha.melanis@yandex.ru>
2023-03-28 00:26:04 +0300

    Bmi code is correct

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,9 @@
 public class BmiService {
+    public int index(int weight, double height) {
+        int total;
+        total = (int) (weight / Math.pow(height, 2));
+        return total;
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       int height = (int) Math.pow(1.87, 2);
-       int weight = 98;
+        BmiService calculate = new BmiService();
+        int myIndex = calculate.index(98, 1.87);
 
-       System.out.println(weight / height);
+        System.out.println(myIndex);
+
 
     }
 }
