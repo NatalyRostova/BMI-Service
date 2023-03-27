35762f405bb6d27745c18c7c262f976527a7321f
Nata R <natasha.melanis@yandex.ru>
2023-03-28 00:38:24 +0300

    Test of double type

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService calculate = new BmiService();
-        int myIndex = calculate.index(98, 1.87);
+        int myIndex = calculate.index((int) 67.3, 1.65);
+
+        //weight - вес в кг
+        //height - рост в м
 
         System.out.println(myIndex);
 
